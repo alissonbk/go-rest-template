4731cc01f4a7e767d0d7fec20b35c1a2f93963d0
alissonbk <alissonbk2010@hotmail.com>
2024-02-10 17:01:48 -0300

    feat: config db pool of connections

--- config/database.go
+++ config/database.go
@@ -5,16 +5,34 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
+	"strconv"
+	"time"
 )
 
 func ConnectDB() *gorm.DB {
 	var err error
 	dsn := os.Getenv("DB_DSN")
+	maxOpen, err := strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONN"))
+	if err != nil {
+		log.Fatal("ENV DB_MAX_OPEN_CONN should be an integer!!")
+	}
+	maxIdle, err := strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONN"))
+	if err != nil {
+		log.Fatal("ENV DB_MAX_IDLE_CONN should be an integer!!")
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error while connecting to database, Error: ", err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Error while acquiring sql.DB from gorm lib, Error: ", err)
+	}
+	sqlDB.SetMaxOpenConns(maxOpen)
+	sqlDB.SetMaxIdleConns(maxIdle)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	return db
 }
