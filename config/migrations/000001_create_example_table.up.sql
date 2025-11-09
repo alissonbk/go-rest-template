b33b87722ef1158d7ceaf317803b58a7a95a63d3
alissonbk <alissonbk2010@hotmail.com>
2025-11-09 09:37:23 -0300

    fix: migration to example

--- config/migrations/000001_create_example_table.up.sql
+++ config/migrations/000001_create_example_table.up.sql
@@ -1,9 +1,6 @@
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS "example" (
     id bigint generated always as identity primary key,
-    name text,
-    email text,
-    password text,
-    is_active boolean not null default true,
+    name text,    
     created_at timestamptz not null default now()::timestamptz,
     updated_at timestamptz,
     deleted_at timestamptz
